50d35cee068a1d59d516a094bb030871c3c99201
kraev dmitriy <kraevdmitriy.zamt@gmail.com>
2024-12-02 13:34:21 +0300

    close #2 Добавлнена функция востановления структуры БД из .sql файла

---
+++ curse/LocalAdminForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace curse
+{
+    public partial class LocalAdminForm : Form
+    {
+        public LocalAdminForm()
+        {
+            InitializeComponent();
+            this.ControlBox = false;
+        }
+
+        private void LocalAdminForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "SQL Files (*.sql)|*.sql";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    RestoreDatabaseStructure(filePath);
+                }
+            }
+        }
+
+        private void RestoreDatabaseStructure(string filePath)
+        {
+            string sqlScript = File.ReadAllText(filePath);
+
+            dbhelper.InsertDataOnDb(sqlScript);
+        }
+    }
+}
