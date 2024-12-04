067600f5bcfdf1451d63175595033e16f7989f3f
kraev dmitriy <kraevdmitriy.zamt@gmail.com>
2024-12-04 11:05:08 +0300

    close #11

--- curse/AuthForm.cs
+++ curse/AuthForm.cs
@@ -14,6 +14,14 @@ namespace curse
     public partial class AuthForm : Form
     {
         private static int counter = 0;
+
+        private static string currentcapcha = String.Empty;
+        Bitmap capchaImg = null;
+
+        PictureBox PB = new PictureBox { Top = 10, Left = 230 };
+        TextBox capchaTextBox = new TextBox { Left = 230, Top = 95 };
+        Button confirmCapchaButton = new Button { Left = 230, Top = 130, Width = 100 };
+
         public AuthForm()
         {
             InitializeComponent();
@@ -24,7 +32,7 @@ namespace curse
 
             string login = textBox1.Text;
             string password = textBox2.Text;
-            if (counter < 1)
+            if (counter < 0)
             {
                 if (login == LocalAdminAccount.Username && password == LocalAdminAccount.Password)
                 {
@@ -62,19 +70,41 @@ namespace curse
             else {
                 this.Width = 400;
 
-                string capcha = Capcha.GenerateCaptcha();
-                Bitmap capchaImg = Capcha.DrawCaptcha(capcha);
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
 
-                PictureBox PB = new PictureBox { Top = 10, Left = 230 };
+
+                currentcapcha = Capcha.GenerateCaptcha();
+                Bitmap capchaImg = Capcha.DrawCaptcha(currentcapcha);
                 PB.Image = capchaImg;
 
-                TextBox capchaTextBox = new TextBox { Left = 230, Top = 95 };
+
                 capchaTextBox.Font = new Font(label1.Font.Name, 14.25F);
+                int a = capchaTextBox.Width;
 
+                
+                confirmCapchaButton.Text = "Проверить";
+                confirmCapchaButton.Click += ConfirmCapchaButton_Click;
 
-
-                this.Controls.Add(capchaTextBox);
                 this.Controls.Add(PB);
+                this.Controls.Add(capchaTextBox);
+                this.Controls.Add(confirmCapchaButton);
+            }
+        }
+
+        private void ConfirmCapchaButton_Click(object sender, EventArgs e)
+        {
+            if (currentcapcha == capchaTextBox.Text) {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+
+                this.Width = 201;
+
+                //this.Controls.Remove(capchaTextBox);
+                //this.Controls.Remove(PB);
+                //this.Controls.Remove(confirmCapchaButton);
+
+                this.AuthForm_Load(sender, e);
             }
         }
 
@@ -87,6 +117,9 @@ namespace curse
         private void AuthForm_Load(object sender, EventArgs e)
         {
             counter = 0;
+            currentcapcha = Capcha.GenerateCaptcha();
+            Bitmap capchaImg = Capcha.DrawCaptcha(currentcapcha);
+            PB.Image = capchaImg;
         }
     }
 }
